bd5280d41110c802213ac99c86960fc6aa5784f4
Debarghya <mondaldebarghya34@gmail.com>
2021-07-03 10:07:28 +0530

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
     <div className="footer-container">
       <section className="footer-subscription">
         <p className="footer-subscription-heading">
-          Join the Adventure newsletter to receive our best vacation deals
+          Join our monthly newsletter to receive our best deals
         </p>
         <p className="footer-subscription-text">
           You can unsubscribe at any time.
